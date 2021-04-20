8612132f1d753e377b76e8fc68d61798cfd38ca6
Andrey Silva <AndreyFreitag2001@outlook.com>
2021-04-20 17:05:04 -0300

    sla

---
+++ Calculadora.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace CalculadoraPOO.ConsoleApp
+{
+    public class Calculadora
+    {
+        public double Numero1;
+        public double Numero2;
+        public double Resultado;
+        public string[] Historico = new string[30];
+        public int contador = 0;
+        public void Somar()
+        {
+            Console.Clear();
+            Console.Write("Digite o primeiro número: "); Numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");  Numero2 = System.Convert.ToDouble(Console.ReadLine());
+            Resultado = Numero1 + Numero2;
+            Historico[contador] = $"{Numero1} + {Numero2} = {Resultado}";
+            Console.WriteLine(Historico[contador]);
+            Console.ReadLine();
+            Console.Clear();
+            contador++;
+        }
+
+        public void Subtrair()
+        {
+            Console.Clear();
+            Console.Write("Digite o primeiro número: "); Numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");  Numero2 = System.Convert.ToDouble(Console.ReadLine());
+            Resultado = Numero1 - Numero2;
+            Historico[contador] = $"{Numero1} - {Numero2} = {Resultado}";
+            Console.WriteLine(Historico[contador]);
+            Console.ReadLine();
+            Console.Clear();
+            contador++;
+        }
+
+        public void Multiplicar()
+        {
+            Console.Clear();
+            Console.Write("Digite o primeiro número: "); Numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");  Numero2 = System.Convert.ToDouble(Console.ReadLine());
+            Resultado = Numero1 * Numero2;
+            Historico[contador] = $"{Numero1} * {Numero2} = {Resultado}";
+            Console.WriteLine(Historico[contador]);
+            Console.ReadLine();
+            Console.Clear();
+            contador++;
+        }
+
+        public void Dividir()
+        {
+                Console.Clear();
+                Console.Write("Digite o primeiro número: "); Numero1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Digite o segundo número: ");  Numero2 = System.Convert.ToDouble(Console.ReadLine());
+            if (Numero2 != 0)
+            {
+                Resultado = Numero1 / Numero2;
+                Historico[contador] = $"{Numero1} / {Numero2} = {Resultado}";
+                Console.WriteLine(Historico[contador]);
+                Console.ReadLine();
+                Console.Clear();
+                contador++;
+            }
+            else
+            {
+                Console.WriteLine("Não é possivel fazer essa operação.");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        public void HistoricoOp()
+        {
+            for (int i = 0; i < contador; i++)
+            {              
+                System.Console.WriteLine(Historico[i]);
+            }
+            System.Console.ReadLine();
+            System.Console.Clear();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPOO.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "";
+            Calculadora calculadora = new Calculadora();
+
+            while (true)
+            {
+                opcao = ObterOpcao();
+                ObterOperacao(opcao, calculadora);
+
+                if (OpcaoSair(opcao))
+                {
+                    break;
+                }
+                if (OpcaoInvalida(opcao))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Opção inválida!");
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+            }
+        }
+
+        private static void ObterOperacao(string opcao, Calculadora calculadora)
+        {
+            switch (opcao)
+            {
+                case "1":                  
+                    calculadora.Somar();
+                    break;
+
+                case "2":  
+                    calculadora.Subtrair();
+                    break;
+
+                case "3":
+                    calculadora.Multiplicar();
+                    break;
+
+                case "4":
+                    calculadora.Dividir();
+                    break;
+
+                case "5":
+                    Console.Clear();
+                    calculadora.HistoricoOp();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private static bool OpcaoSair(string opcao)
+        {
+            return opcao.Equals("s", StringComparison.OrdinalIgnoreCase);
+        }
+
+        private static bool OpcaoInvalida(string opcao)
+        {
+            return opcao != "1" && opcao != "2" && opcao != "3" && opcao != "4" && opcao != "5";
+        }
+
+        private static string ObterOpcao()
+        {
+            string opcao;
+            Console.WriteLine("Digite qual operação você irá fazer"); Console.WriteLine("(1) SOMA"); Console.WriteLine("(2) SUBTRAÇÃO"); Console.WriteLine("(3) MULTIPLICAÇÃO"); Console.WriteLine("(4) DIVISÃO"); Console.WriteLine("(5) VISUALIZAR");
+
+            opcao = Console.ReadLine();
+            return opcao;
+        }
+    }
+}
